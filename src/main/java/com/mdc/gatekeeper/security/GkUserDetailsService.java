f6884b80a4f0c2f8ecd284f8cf03c4325ba0341b
CaDyMaN <dragu.costel@gmail.com>
2025-01-06 18:43:40 +0200

    Fix configuration for security

---
+++ src/main/java/com/mdc/gatekeeper/security/GkUserDetailsService.java
@@ -0,0 +1,14 @@
+package com.mdc.gatekeeper.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GkUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
